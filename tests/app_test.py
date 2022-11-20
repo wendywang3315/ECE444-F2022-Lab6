48cd1153a4deb3976b18d5446a8bb7d9a9118028
wendywang3315 <yutong_wang3315@outlook.com>
2022-11-19 23:43:56 -0500

    finished First Test and Database Setup

---
+++ tests/app_test.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+from project.app import app, init_db
+
+def test_index():
+    tester = app.test_client()
+    response = tester.get("/", content_type="html/text")
+
+    assert response.status_code == 200
+    assert response.data == b"Hello, World!"
+
+def test_database():
+    assert Path("flaskr.db").is_file()
